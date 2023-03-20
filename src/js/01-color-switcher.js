35d23d32ddfe5972706d7c3b93d8b7a0bb809b11
Iryna Zaliska <zaliska@gmail.com>
2023-03-21 01:17:17 +0200

    hw-09-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStopEl.disabled = true;
+
+btnStartEl.addEventListener('click', (event) => {
+  event.target.disabled = true;
+  btnStopEl.disabled = false;
+
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStopEl.addEventListener('click', (event) => {
+  event.target.disabled = true;
+  btnStartEl.disabled = false;
+
+  clearInterval(intervalId);
+});
