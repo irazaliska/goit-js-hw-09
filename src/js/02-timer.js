b15b1f7c83ed0729e0ee5dcbf5dca421b053b222
Iryna Zaliska <zaliska@gmail.com>
2023-03-22 01:58:31 +0200

    hw-09-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputEl = document.querySelector('#datetime-picker');
 const btnStartEl = document.querySelector('[data-start]');
@@ -11,13 +11,55 @@ const secEl = document.querySelector('[data-seconds]');
 
 btnStartEl.disabled = true;
 
+let intervalId = null;
+let selectedDate = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose,
+  onClose(selectedDates) {
+    validateDate(selectedDates); 
+  },
 };
+
+flatpickr(inputEl, options);
+
+function validateDate(selectedDates) {
+
+  selectedDate = selectedDates[0].getTime();
+
+  if (selectedDate <= new Date().getTime()) {
+
+    return Notify.failure('Please choose a date in the future');
+  } else {
+    btnStartEl.disabled = false;
+    btnStartEl.addEventListener('click', () => {
+      onStartTimer(selectedDate);
+    });
+  }
+  }
+
+function onStartTimer() {
+  intervalId = setInterval(() => {
+    const timerTime = selectedDate - new Date().getTime();
+    stopInterval(timerTime);
+    
+    const { days, hours, minutes, seconds } = convertMs(timerTime);
+    dayEl.textContent = addLeadingZero(days);
+    hourEl.textContent = addLeadingZero(hours);
+    minEl.textContent = addLeadingZero(minutes);
+    secEl.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+function stopInterval(timerTime) {
+  if (timerTime < 1000) {
+    clearInterval(intervalId);
+    Notify.success(`Time is over`);
+  }
+}
 
 function convertMs(ms) {
   const second = 1000;
@@ -33,3 +75,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
