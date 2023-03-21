c73cafba9650bec6243754e70fba82402502c142
Iryna Zaliska <zaliska@gmail.com>
2023-03-21 17:54:49 +0200

    init interval

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,6 +6,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+let intervalId = null;
 btnStopEl.disabled = true;
 
 btnStartEl.addEventListener('click', (event) => {
