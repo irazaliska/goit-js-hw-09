df1723b5f89910319087f4f2b12f9a7a5b5eaf8a
Iryna Zaliska <zaliska@gmail.com>
2023-03-22 19:53:30 +0200

    hw-09-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const delay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+
+  if (delay < 0 || step < 0 || amount <= 0) {
+    Notify.warning('Please fill with positive numbers');
+    return;
+  }
+
+  let currentDelay = delay;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += step;
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
